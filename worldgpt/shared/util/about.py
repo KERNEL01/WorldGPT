57ee0d8705f1b2426af8094418d0f00c3541bd7a
rich <rich@pyrge.games>
2023-05-07 16:40:47 +1000

    add about utilities

---
+++ worldgpt/shared/util/about.py
@@ -0,0 +1,20 @@
+from worldgpt.shared.util.git_version import *
+
+
+""" About utilities, constant vars. """
+
+__TITLE__ = 'WorldGPT'
+__AUTHOR__ = 'rich'
+__MAINTAINER__ = 'rich'
+__CREDITS__ = ['rich']
+__EMAIL__ = 'rich@pyrge.games'
+__CONTACT__ = {"discord": "rich#0008"}
+
+__LICENSE__ = "CC Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0) " \
+              "https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode "
+__COPYRIGHT__ = "Copyright 2023, Rich@pyrge.games"
+
+
+__VERSION__ = get_version()
+__STATUS__ = get_tag()
+
