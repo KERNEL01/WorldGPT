9730c1915595a5c04cd9af17b293fd40d31296ae
rich <rich@pyrge.games>
2023-06-03 09:40:11 +1000

    turned on hot-reload, added initial Character api endpoints, fixed api not observing configs correctly.

--- worldgpt/server/subsystem/api.py
+++ worldgpt/server/subsystem/api.py
@@ -15,6 +15,8 @@
 
 import os
 from fastapi import FastAPI
+
+from worldgpt.shared.model.character import Character
 from worldgpt.shared.util import about
 
 
@@ -29,9 +31,48 @@ def root():
             "author": about.__AUTHOR__}
 
 
+@application.get("/characters")
+def get_characters():
+    """ Returns a list of all characters."""
+    from worldgpt.server.subsystem.database import Database
+    output = {}
+    with Database().lock.r_locked():
+        characters = dict(Database().characters)
+    for character in characters:
+        output[character] = characters[character].dict()
+    return output
+
+
+@application.post('/characters/new')
+def create_character(character: Character):
+    """ Creates a new character."""
+    from worldgpt.server.subsystem.database import Database
+    with Database().lock.r_locked():
+        characters = dict(Database().characters)
+    if character.name in characters:
+        return {'error': 'Character already exists.'}
+
+
+@application.delete('/characters/{name}')
+def delete_character(name: str):
+    """ Deletes a character."""
+    from worldgpt.server.subsystem.database import Database
+    with Database().lock.r_locked():
+        characters = dict(Database().characters)
+    if name not in characters:
+        return {'error': 'Character does not exist.'}
+    # todo deletion request to queue. return {'success': 'Character deleted.'}
+    return {'error': 'Not implemented.'}
+
+
 def run_in_main_thread():
     import uvicorn
-    uvicorn.run(application,
-                host=os.environ.get('wgpt_listen_address', '127.0.0.1'),
-                port=os.environ.get('wgpt_listen_port', 8000)
+    from worldgpt.server.subsystem.configuration import Configuration
+    with Configuration().lock.r_locked():
+        api_listen_host = Configuration().api_listen_host or "127.0.0.1"
+        api_listen_port = Configuration().api_listen_port or 8000
+    uvicorn.run('worldgpt.server.subsystem.api:application',
+                host=os.environ.get('wgpt_listen_address', api_listen_host),
+                port=os.environ.get('wgpt_listen_port', api_listen_port),
+                reload=True
                 )
