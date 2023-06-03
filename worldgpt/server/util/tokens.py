b3b738f736cd479d0471749c1b06db3c92f75eef
rich <rich@pyrge.games>
2023-06-04 05:13:44 +1000

    moved token counter utility to server/utils.

--- worldgpt/server/util/tokens.py
+++ worldgpt/server/util/tokens.py
@@ -1,7 +1,7 @@
 import tiktoken
 
 
-def count_prompt_tokens(message: str, model: str = "gpt-4"):
+def count_prompt_tokens(message: str, model: str = "gpt-3.5-turbo"):
     """ Counts the number of tokens in a prompt using tiktoken. """
     encoder = tiktoken.encoding_for_model(model)
     return len(encoder.encode(message))
