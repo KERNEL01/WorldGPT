ed0b8fafa87701b379b4dfc4a194d6231bcc2c5c
rich <rich@pyrge.games>
2023-06-04 05:12:44 +1000

    required the role to be one of three accepted values in OpenAI. added method for stripping timestamp information from timestamp var.

--- worldgpt/shared/model/message.py
+++ worldgpt/shared/model/message.py
@@ -1,10 +1,20 @@
 import datetime
+from typing import Literal
 
 from pydantic import BaseModel
 
 
 class Message(BaseModel):
     """ A message object for use in issuing information to the language model. """
-    role: str
+    role: Literal['user', 'system', 'assistant']  # openai specific
     content: str
     timestamp: float = float(datetime.datetime.now().timestamp())
+
+    def to_openai(self, preserve_timestamp=False):
+        """ Return a dictionary that can be used with OpenAI api calls. Timestamp is used only by WorldGPT. """
+        message = self.dict()
+        if preserve_timestamp:
+            time_str = datetime.datetime.fromtimestamp(self.timestamp).strftime("%Y-%m-%d %H:%M:%S")
+            message['content'] = f"{time_str}: {self.content}"
+        message.pop("timestamp")
+        return message
